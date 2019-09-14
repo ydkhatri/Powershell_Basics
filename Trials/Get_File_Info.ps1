bc4a5188a30c221b767c7165f636eeece89b2de0
Jacob <jake@jwhite.network>
2019-09-14 15:28:23 -0400

    Added basic trials to get started

---
+++ Trials/Get_File_Info.ps1
@@ -0,0 +1,17 @@
+# Getting File Information
+
+# Story Line: Basic Folder and File information Gathering
+
+
+# Please write and run the command that you would need to get all the files in your home directory (Typically at C:\Users\<USERNAME>)
+
+
+
+# Modify the command so that it would get all hidden files and recurse through all sub-folders. (You do not have to run it because it can take some time if you have a lot of files.)
+
+
+
+# Modify the command so that it prints the name and last write time for each file in the your home directory. Set a variable equal to the command. Hint: foreach and Get-Member
+
+
+
---
+++ Trials/Loops.ps1
@@ -0,0 +1,17 @@
+# Expanding on Getting File Information. Please complete Get_File_Info.ps1 first.
+
+# Story Line: Using if statements for advanced filtering.
+
+
+# Modifying the Get-ChildItem cmdlet, how would you only get .exe files in C:\Windows\ with no recursion.
+
+
+
+# Modify the script so that it print the file name and size (called length).
+
+
+
+# Using an if statement, make so only exe files are only printed if they are over 1 MB and get the SHA512 hash of them.
+
+
+
